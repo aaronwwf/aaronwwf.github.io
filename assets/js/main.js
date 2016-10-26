65e2e11cbe5207dda25932bbcdca03120974aa5b
aaronwwf <dcswuw@gmail.com>
2016-10-26 20:31:44 +0800

    add multiple language surport

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,18 @@ jQuery(document).ready(function($) {
             }, 800);
             
         });
-
+        changeLanguage("en");
     });
-   
+    window.currentLan="en";
+    function changeLanguage(lan){
+    	$("."+window.currentLan).hide();
+    	$("."+lan).show();
+    	window.currentLan=lan;
+    } 
+    $(".changeLan").click(function(){
+    	var lan = $(this).attr("lan");
+    	changeLanguage(lan);
+    	return false;
+    });
     
-
 });
